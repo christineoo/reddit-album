d24a0312b9e996b2c842665c4ccd69105cbee5d9
Christine Oo <ootjinvoon@gmail.com>
2019-02-03 19:02:48 +0900

    add test

---
+++ src/components/__tests__/Navigation.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { cleanup, render } from "react-testing-library"
+
+import Navigation, { SUBREDDITS } from "../Navigation"
+
+afterEach(cleanup)
+
+test("value and label of navigation", () => {
+  const { getByTestId } = render(<Navigation />)
+  SUBREDDITS.map((subreddit) => {
+    expect(getByTestId(subreddit.value).textContent).toEqual(subreddit.label)
+  })
+})
+
+test("href value is prefix with `/r/`", () => {
+  const { getByTestId } = render(<Navigation />)
+  SUBREDDITS.map((subreddit) => {
+    expect(getByTestId(subreddit.value).getAttribute("href")).toEqual(`/r/${subreddit.value}`)
+  })
+})
