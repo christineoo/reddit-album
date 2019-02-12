6903d105c53fe589c8e552b1c2f5136932f21521
Christine Oo <ootjinvoon@gmail.com>
2019-02-12 16:52:15 +0900

    fix Jest encountered an unexpected token

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,48 @@
 module.exports = {
-    transform: {
-        "^.+\\.tsx?$": "ts-jest",
+  coveragePathIgnorePatterns: [
+    '.*\\.d\\.ts',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  testEnvironment: 'node',
+  testURL: 'http://localhost',
+  testMatch: [
+    '<rootDir>/src/**/?(*.)(spec|test).ts?(x)',
+    '<rootDir>/src/**/__tests__/**/*.ts?(x)',
+  ],
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\](?!(lodash-es|reach-router-hash-history)/.*).+\\.(js|jsx|ts|tsx)$',
+  ],
+    moduleNameMapper: {
+    '^.+\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'node',
+    'ts',
+    'tsx',
+    'web.js',
+    'web.jsx',
+    'web.ts',
+    'web.tsx',
+  ],
+  globals: {
+    window: {},
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    testPathIgnorePatterns: ["/lib/", "/node_modules/"],
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverage: true,
-};
+  },
+  reporters: [
+    'default',
+  ],
+  preset: 'ts-jest/presets/js-with-ts',
+}
