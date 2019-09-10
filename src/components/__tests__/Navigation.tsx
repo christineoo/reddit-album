980f25526d5e9acf57e826cdd6c812771b7ac538
Christine Oo <ootjinvoon@gmail.com>
2019-09-10 23:08:51 +0900

    add tests

--- src/components/__tests__/Navigation.tsx
+++ src/components/__tests__/Navigation.tsx
@@ -5,16 +5,18 @@ import Navigation, { SUBREDDITS } from "../Navigation"
 
 afterEach(cleanup)
 
-test("value and label of navigation", () => {
-  const { getByTestId } = render(<Navigation />)
-  SUBREDDITS.map((subreddit) => {
-    expect(getByTestId(subreddit.value).textContent).toEqual(subreddit.label)
+describe("Navigation Component", () => {
+  test("value and label of navigation", () => {
+    const { getByTestId } = render(<Navigation />)
+    SUBREDDITS.map((subreddit) => {
+      expect(getByTestId(subreddit.value).textContent).toEqual(subreddit.label)
+    })
   })
-})
 
-test("href value is prefix with `/r/`", () => {
-  const { getByTestId } = render(<Navigation />)
-  SUBREDDITS.map((subreddit) => {
-    expect(getByTestId(subreddit.value).getAttribute("href")).toEqual(`/r/${subreddit.value}`)
+  test("href value is prefix with `/r/`", () => {
+    const { getByTestId } = render(<Navigation />)
+    SUBREDDITS.map((subreddit) => {
+      expect(getByTestId(subreddit.value).getAttribute("href")).toEqual(`/r/${subreddit.value}`)
+    })
   })
 })
