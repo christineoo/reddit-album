5beafb09a6edea99f4adc98d1de40cd0940fb1ae
Christine Oo <ootjinvoon@gmail.com>
2022-11-27 14:56:09 +0900

    use vitejs

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite"
+// import { createHtmlPlugin } from 'vite-plugin-html';
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+  root: "src",
+  build: {
+    // Relative to the root
+    outDir: "../dist"
+  },
+  plugins: [
+    // createHtmlPlugin({
+    //   inject: {
+    //     data: {
+    //       title: env === 'production' ? 'My site' : `My site [${env.toUpperCase()}]`,
+    //     },
+    //   },
+    // }),
+    react({
+      // Use React plugin in all *.jsx and *.tsx files
+      include: "**/*.{jsx,tsx}",
+      jsxRuntime: "classic"
+    })
+  ],
+  base: "/reddit-album/"
+})
